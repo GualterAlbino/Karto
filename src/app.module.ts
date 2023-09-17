29126a47f0d0f25d48c2dd7d8536acbf63d14e72
Gualter Albino <gualteralbino1000@gmail.com>
2023-09-17 16:06:00 -0300

    Configurações do Banco de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfig } from './config/postgres.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(
+			//Importa o módulo de configuração
+			{ isGlobal: true }, //Define que as configurações são globais
+		),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfig, //Aponta a classe de config
+      inject: [PostgresConfig] ////Injeta a classe de config
+    })
+	],
+	
 })
 export class AppModule {}
