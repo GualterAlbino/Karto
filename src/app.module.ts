7ab94c9a75dc841d703a7ef49de269c70ea55ab6
Gualter Albino <gualteralbino1000@gmail.com>
2023-09-19 17:29:44 -0300

    Tratamento generico de erros

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfig } from './config/postgres.config';
+import { UsuarioModule } from '../src/modules/usuario/usuario.module';
+import { APP_FILTER } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './middlewares/filtro-de-excecao-http';
 
 @Module({
 	imports: [
@@ -12,8 +15,10 @@ import { PostgresConfig } from './config/postgres.config';
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfig, //Aponta a classe de config
       inject: [PostgresConfig] ////Injeta a classe de config
-    })
+    }),
+    UsuarioModule
 	],
+	providers: [{ provide: APP_FILTER, useClass: FiltroDeExcecaoHttp }],
 	
 })
 export class AppModule {}
