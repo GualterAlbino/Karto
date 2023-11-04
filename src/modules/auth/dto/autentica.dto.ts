e82b606c4788ce23cabe7cf30eb01f0d69f5ef6c
Gualter Albino <gualteralbino1000@gmail.com>
2023-11-04 14:20:34 -0300

    no message

--- src/modules/auth/dto/autentica.dto.ts
+++ src/modules/auth/dto/autentica.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class AutenticaDTO {
+	@ApiProperty()
 	@IsEmail(undefined, { message: 'Email inválido!' })
 	email: string;
 
+	@ApiProperty()
 	@IsNotEmpty({ message: 'A senha não pode ser vazia!' })
 	senha: string;
 }
