77d54612970a9f220bedbd26a77645745e3a8893
Gualter Albino <gualteralbino1000@gmail.com>
2023-11-19 11:55:30 -0300

    Atualiza Versão

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 		.setDescription(
 			'Rotas disponibilizadas...',
 		)
-		.setVersion('0.0.9')
+		.setVersion('0.0.10')
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
